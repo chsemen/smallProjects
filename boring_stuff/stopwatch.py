8ebf68939cafb9f8dd1bdb31fc2122aeff347f45
Semen <ch-semen@rambler.ru>
2025-04-07 11:36:37 +0300

    p402 fix

--- boring_stuff/stopwatch.py
+++ boring_stuff/stopwatch.py
@@ -15,8 +15,8 @@ try:
         input()
         lapTime = round(time.time() - lastTime, 2)
         totalTime = round(time.time() - startTime, 2)
-        # print(f'Lap #{lapNum}: {totalTime} {lapTime}', end='')
-        print(f'Lap #{lapNum}: {totalTime} {lapTime}')
+        print(f'Lap #{lapNum}: {totalTime} {lapTime}', end='')
+        # print(f'Lap #{lapNum}: {totalTime} {lapTime}')
         lapNum += 1
         lastTime = time.time()
 except KeyboardInterrupt:
